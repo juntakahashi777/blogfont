1871a1db68f0d5e8a906e67e0c33b9e985e3a3bd
Jun Takahashi <jtakahas@princeton.edu>
2015-07-07 15:17:15 -0700

    get textarea value

--- handlers.js
+++ handlers.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	$('#newPost').on('submit', function() {
-		alert('hello');
+	$('#newPost').on('submit', function(e) {
+		var postBody = $('#postBody').val();
+		console.log(postBody);
+		e.preventDefault();
 	});
 });
