faef51cf90aa476b7f3f7e3343f43e73eef68f7e
Uts <usikder@princeton.edu>
2015-07-07 21:14:30 -0400

    fix conflicts

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-var http = require('http'),
-    fs = require('fs'),
-    MongoClient = require('mongodb').MongoClient,
-    ObjectId = require('mongodb').ObjectID,
-    assert = require('assert'),
-    url = 'mongodb://localhost:27017/test';
-
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log('connected to mongodb');
-    db.collection('test').insert(
-    {
-        item: "abc"
-    });
-    db.close();
-});
-
-http.createServer(function (req, res) {
-    filename = './homepage.html'
-    fs.readFile(filename, 'binary', function(err, file) {
-        if (err)
-        {
-            res.writeHead(500);
-            res.write(err + '\n');
-            res.end();
-            return;
-        }
-        res.writeHead(200);
-        res.write(file, 'binary');
-        res.end();
-    });
-=======
 var sys = require('sys'),
 	http = require('http'),
 	path = require('path'),
@@ -70,7 +37,6 @@ app.get('/', function(req, res) {
 		res.end();
 	});
 });
->>>>>>> 1871a1db68f0d5e8a906e67e0c33b9e985e3a3bd
 
 app.listen(8000, 'localhost');
 console.log('Server running at http://127.0.0.1:8000/');
